95bd8e0aabf92502742c94c68320096c75a858a0
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-24 15:55:36 -0500

    data update

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -18,6 +18,97 @@ const data = {
         price: 3,
         image: "./images/bananas/Lady-Finger-Banana.jpeg",
       },
+      {
+        id: "4",
+        name: "Red Bananas",
+        price: 4.15,
+        image: "./images/bananas/Red-Bananas.jpeg",
+      },
+      {
+        id: "5",
+        name: "Blue java",
+        price: 4.25,
+        image: "./images/bananas/Blue-Java-banana.jpeg",
+      },
+      {
+        id: "6",
+        name: "Gros Michel",
+        price: 5.49,
+        image: "./images/bananas/Gros-Michel-Banana.jpeg",
+      },
+      {
+        id: "7",
+        name: "Dwarf Jamaican",
+        price: 7.49,
+        image: "./images/bananas/Dwarf-Jamaican-Banana.jpg",
+      },
+      {
+        id: "8",
+        name: "Nanjangud Rasabalehannu",
+        price: 9,
+        image: "./images/bananas/Nanjangud-banana.jpg",
+      },
+      {
+        id: "9",
+        name: "Macho Plantain",
+        price: 3.49,
+        image: "./images/bananas/Macho-Plantain-Banana.jpeg",
+      },
+      {
+        id: "10",
+        name: "Fehi",
+        price: 8.49,
+        image: "./images/bananas/Fehi-Banana.jpg",
+      },
+      {
+        id: "11",
+        name: "Praying Hands",
+        price: 11,
+        image: "./images/bananas/Praying-Hands-Banana.jpg",
+      },
+      {
+        id: "12",
+        name: "Orinoco",
+        price: 15.49,
+        image: "./images/bananas/Orinoco-Banana.jpeg",
+      },
+      {
+        id: "13",
+        name: "Lady Finger Single",
+        price: 2,
+        image: "./images/bananas/Ladys-Finger-Bananas.jpg",
+      },
+      {
+        id: "14",
+        name: "Manzano Single",
+        price: 4,
+        image: "./images/bananas/Manzano-banana.jpg",
+      },
+      {
+        id: "15",
+        name: "Bluggoe Single",
+        price: 8,
+        image: "images/bananas/Bluggoe-Banana.jpeg",
+      },
+      {
+        id: "16",
+        name: "Lets Go Bananas Plush",
+        price: 30,
+        image: "./images/bananas/letsgobananas.jpeg",
+      },
+      {
+        id: "17",
+        name: "Kawaii Banana",
+        price: 40,
+        image: "./images/bananas/kawaiibanana.jpeg",
+      },
+      {
+        id: "18",
+        name: "Duct Tape Banana NFT",
+        price: 121500,
+        image: "./images/bananas/banananft.jpeg",
+      },
+
     ],
   };
   export default data;
